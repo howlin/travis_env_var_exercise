617b82b4558fefcd961930f826f096db1793f12d
Pádraig Howlin <padraig@howlin.ie>
2020-05-05 15:26:58 +0100

    Udacity Exercise: Environment Variables and Travis

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
+    const favoriteFood = process.env.FAVORITE_FOOD;
+    
     console.log("Microservices rock!");
+    console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
